c815e67aab71c1de6537d267f5286256762291a4
Nathan Gillespie <38772830+speedmonster45@users.noreply.github.com>
2020-12-02 05:21:33 -0500

    Add files via upload

--- svm_classifier.py
+++ svm_classifier.py
@@ -36,8 +36,8 @@ lines = [line for line in f.readlines()]
 f.close()
 pathname: str = lines[1]
 img = cv2.imread(pathname)
-cv2.imshow("images", img)
-cv2.waitKey(0)
+#cv2.imshow("images", img)
+#cv2.waitKey(0)
 #imgContour = img.copy()
 #imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
